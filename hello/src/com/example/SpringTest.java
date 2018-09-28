358d70a972646ecca0a90048fd520de2a448ac82
houdongdong <houdongdong@rongzi.com>
2018-09-28 14:04:31 +0800

    使用 @Resource 配置依赖

--- hello/src/com/example/SpringTest.java
+++ hello/src/com/example/SpringTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.service.Chinese;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,5 +13,8 @@ public class SpringTest {
 
         // 使用 @Component 标注一个普通的 Spring Bean 类
         System.out.println("-----------" + Arrays.toString(context.getBeanDefinitionNames()));
+
+        // 使用 @Resource 配置依赖，将 steelAxe 注入到 setter 方法
+        context.getBean("chinese", Chinese.class).useAxe();
     }
 }
--- hello/src/com/example/service/Chinese.java
+++ hello/src/com/example/service/Chinese.java
@@ -2,14 +2,18 @@ package com.example.service;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.*;
 
 // 使用 @Component 标注一个普通的 Spring Bean 类
 // 未指定 id 时， 会使用默认的 id, 即：chinese
 @Component
 public class Chinese implements Person {
+    // @Resource 也可以直接用在 Field 上
     private Axe axe;
 
+    // 使用 @Resource 配置依赖，将 steelAxe 注入到 setter 方法
+    @Resource(name = "steelAxe")
     public void setAxe(Axe axe) {
         this.axe = axe;
     }
