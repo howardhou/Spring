6fda03728cd3e51c4f753c3218cca6e87129bbb8
houdongdong <houdongdong@rongzi.com>
2018-09-28 13:34:52 +0800

    指定Bean的作用域 @Scope("prototype")

--- hello/src/com/example/service/SteelAxe.java
+++ hello/src/com/example/service/SteelAxe.java
@@ -1,7 +1,9 @@
 package com.example.service;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope("prototype")
 @Component
 public class SteelAxe implements Axe {
 
